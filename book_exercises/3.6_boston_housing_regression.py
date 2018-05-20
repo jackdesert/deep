3b18ad10b4c78d69120f1e20ed2a86514bec95b7
Jack Desert <jdtzn@riseup.net>
2018-05-20 10:04:36 -0500

    3.6 runs through validation and plots

--- book_exercises/3.6_boston_housing_regression.py
+++ book_exercises/3.6_boston_housing_regression.py
@@ -10,6 +10,10 @@ import numpy as np
 from keras import models
 from keras import layers
 
+import matplotlib
+matplotlib.use('Agg') # Using Agg because it can run without an X-server
+import matplotlib.pyplot as plt
+
 
 (train_data, train_targets), (test_data, test_targets) = boston_housing.load_data()
 
@@ -51,36 +55,49 @@ def build_model():
 k = 4
 all_mae_histories = []
 
-def b_split(num_chunks, validation_index):
-    train_data_chunks    = np.array_split(train_data,    num_chunks)
-    train_targets_chunks = np.array_split(train_targets, num_chunks)
+def b_split(v_index):
 
-    validation_data    = train_data_chunks[validation_index]
-    validation_targets = train_targets_chunks[validation_index]
+    # chunk length
+    clen = len(train_data) // k
 
-    pdb.set_trace()
-    train_data_part = np.concatenate([train_data_chunks[0:validation_index], train_data_chunks[validation_index + 1:]], axis=0)
+    v_start = v_index * clen
+    v_end   = v_start + clen
+    validation_data    = train_data[v_start : v_end]
+    validation_targets = train_targets[v_start : v_end]
 
-    train_targets_part = np.concatenate(train_targets_chunks[0:validation_index],
-                                        train_targets_chunks[validation_index + 1:])
+    train_data_before = train_data[: v_start]
+    train_data_after  = train_data[v_end :]
+    train_data_both   = np.concatenate((train_data_before, train_data_after))
 
-    return((train_data_part, train_targets_part), (validation_data, validation_targets))
+    train_targets_before = train_targets[: v_start]
+    train_targets_after  = train_targets[v_end :]
+    train_targets_both   = np.concatenate((train_targets_before, train_targets_after))
+
+    return((train_data_both, train_targets_both), (validation_data, validation_targets))
 
 
 for i in range(k):
-    (train_data_part, train_targets_part), (validation_data, validation_targets) = b_split(k, i)
+    (train_data_part, train_targets_part), (validation_data, validation_targets) = b_split(i)
     m = build_model()
     history = m.fit(train_data_part,
                     train_targets_part,
-                    epochs=100,
+                    epochs=50,
                     verbose=1,
                     batch_size=1,
                     validation_data=(validation_data, validation_targets))
     mae_history = history.history['val_mean_absolute_error']
     all_mae_histories.append(mae_history)
 
-pdb.set_trace()
 
+
+for color, hist in zip(['bo', 'ro', 'yo', 'ko'], all_mae_histories):
+    plt.plot(hist, color)
+
+mean = np.mean(all_mae_histories, axis=0)
+plt.plot(mean, 'm')
+plt.savefig('/tmp/plot.png')
+
+pdb.set_trace()
 
 
 
